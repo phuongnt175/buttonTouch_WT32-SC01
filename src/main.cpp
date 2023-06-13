419742e3db1b3f4d8b3ecd7e2067c35fe5fb7109
phuongnt175 <phuong98a2@gmail.com>
2023-06-13 09:03:52 +0700

    test AsyncWebServer

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,8 @@
 #include <FT6236.h>
 #include <ui.h>
 #include <WiFi.h>
-#include <WebServer.h>
+#include <ESPAsyncWebServer.h>
+#include <AsyncTCP.h>
 
 #define SDA_FT6236 18
 #define SCL_FT6236 19
@@ -14,7 +15,14 @@ FT6236 ts = FT6236();
 const char *ssid = "LUMI_TEST";
 const char *password = "lumivn274!";
 
-WebServer server(80);
+const char* PARAM_INPUT_1 = "output";
+const char* PARAM_INPUT_2 = "state";
+
+int buttonState;
+int ledState = LOW;
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
 
 extern lv_obj_t * ui_secondButton;
 extern lv_obj_t * ui_firstButton;
@@ -23,6 +31,8 @@ extern lv_obj_t * ui_Screen2;
 extern lv_obj_t * ui_Screen3;
 extern lv_obj_t * ui_Screen4;
 extern lv_obj_t * ui_Colorwheel1;
+extern int brightnessValue;
+
 class LGFX : public lgfx::LGFX_Device
 {
 lgfx::Panel_ST7796     _panel_instance;
@@ -90,34 +100,11 @@ public:
 
 LGFX tft;
 
-bool btn1Status = LOW;
-bool btn2Status = LOW;
-bool btn3Status = LOW;
-bool btn4Status = LOW;
-
-extern int brightnessValue;
-static bool Flag1 = 0;
-static bool Flag2 = 0;
-static bool Flag3 = 0;
-static bool Flag4 = 0;
-
 /*Change to your screen resolution*/
 static const uint32_t screenWidth  = 480;
 static const uint32_t screenHeight = 320;
 static lv_disp_draw_buf_t draw_buf;
 static lv_color_t buf[ screenWidth * 10 ];
-
-void handle_OnConnect();
-void handle_btn1on();
-void handle_btn2on();
-void handle_btn3on();
-void handle_btn4on();
-void handle_btn1off();
-void handle_btn2off();
-void handle_btn3off();
-void handle_btn4off();
-void handle_NotFound();
-String SendHTML(uint8_t btn1stat, uint8_t btn2stat, uint8_t btn3stat, uint8_t btn4stat);
 
 /* Display flushing */
 void my_disp_flush( lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p )
@@ -147,36 +134,80 @@ void my_touchpad_read( lv_indev_drv_t * indev_driver, lv_indev_data_t * data )
    }
 }
 
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+<head>
+  <title>ESP Web Server</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="data:,">
+  <style>
+    html {font-family: Arial; display: inline-block; text-align: center;}
+    h2 {font-size: 3.0rem;}
+    p {font-size: 3.0rem;}
+    body {max-width: 600px; margin:0px auto; padding-bottom: 25px;}
+    .switch {position: relative; display: inline-block; width: 120px; height: 68px} 
+    .switch input {display: none}
+    .slider {position: absolute; top: 0; left: 0; right: 0; bottom: 0; background-color: #ccc; border-radius: 6px}
+    .slider:before {position: absolute; content: ""; height: 52px; width: 52px; left: 8px; bottom: 8px; background-color: #fff; -webkit-transition: .4s; transition: .4s; border-radius: 3px}
+    input:checked+.slider {background-color: #b30000}
+    input:checked+.slider:before {-webkit-transform: translateX(52px); -ms-transform: translateX(52px); transform: translateX(52px)}
+  </style>
+</head>
+<body>
+  <h2>ESP Web Server</h2>
+  %BUTTONPLACEHOLDER%
+<script>function toggleCheckbox(element) {
+  var xhr = new XMLHttpRequest();
+  if(element.checked){ xhr.open("GET", "/update?output="+element.id+"&state=1", true); }
+  else { xhr.open("GET", "/update?output="+element.id+"&state=0", true); }
+  xhr.send();
+}
+
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var inputChecked;
+      var outputStateM;
+      if( this.responseText == 1){ 
+        inputChecked = true;
+        outputStateM = "On";
+      }
+      else { 
+        inputChecked = false;
+        outputStateM = "Off";
+      }
+      document.getElementById("output").checked = inputChecked;
+      document.getElementById("outputState").innerHTML = outputStateM;
+    }
+  };
+  xhttp.open("GET", "/state", true);
+  xhttp.send();
+}, 1000 ) ;
+</script>
+</body>
+</html>
+)rawliteral";
+
+// Replaces placeholder with button section in your web page
+String processor(const String& var){
+  //Serial.println(var);
+  if(var == "BUTTONPLACEHOLDER"){
+    String buttons = "";
+    buttons += "<h4> Living Room Light  </h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"Living Room Light\" " "> <span class=\"slider\"></span></label>";
+    buttons += "<h4> Kitchen Room Light </h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"Kitchen Room Light\" " "><span class=\"slider\"></span></label>";
+    buttons += "<h4> Curtain Mode       </h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"Curtain Mode\" " ">      <span class=\"slider\"></span></label>";
+    buttons += "<h4> AC                 </h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"AC\" " ">                <span class=\"slider\"></span></label>";
+    return buttons;
+  }
+  return String();
+}
+
 void setup()
 {
   Serial.begin(115200);
 
   tft.begin();
-  WiFi.begin(ssid, password);
-
-  while(WiFi.status() != WL_CONNECTED)
-  {
-    delay(1000);
-    Serial.print(".");
-  }
-
-  Serial.println("Wifi connected...!");
-  Serial.print("Got IP: ");
-  Serial.println(WiFi.localIP());
-
-  server.on("/", handle_OnConnect);
-  server.on("/btn1on", handle_btn1on);
-  server.on("/btn2on", handle_btn2on);
-  server.on("/btn3on", handle_btn3on);
-  server.on("/btn4on", handle_btn4on);
-  server.on("/btn1off", handle_btn1off);
-  server.on("/btn2off", handle_btn2off);
-  server.on("/btn3off", handle_btn3off);
-  server.on("/btn4off", handle_btn4off);
-  server.onNotFound(handle_NotFound);
-  server.begin();
-  Serial.println("HTTP server started");
-
   tft.setRotation(1);
   tft.setBrightness(255);
 
@@ -205,54 +236,69 @@ void setup()
   indev_drv.read_cb = my_touchpad_read;
   lv_indev_drv_register(&indev_drv);
 
+  WiFi.begin(ssid, password);
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(10000);
+    Serial.println("Connecting to WiFi...");
+  }
+
+  Serial.println(WiFi.localIP());
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", index_html, processor);
+  });
+
+  // Send a GET request to <ESP_IP>/update?output=<inputMessage1>&state=<inputMessage2>
+  server.on("/update", HTTP_GET, [] (AsyncWebServerRequest *request) {
+  String inputMessage1;
+  String inputMessage2;
+  // GET input1 value on <ESP_IP>/update?output=<inputMessage1>&state=<inputMessage2>
+  if (request->hasParam(PARAM_INPUT_1) && request->hasParam(PARAM_INPUT_2)) {
+    inputMessage1 = request->getParam(PARAM_INPUT_1)->value();
+    inputMessage2 = request->getParam(PARAM_INPUT_2)->value();
+    //digitalWrite(inputMessage1.toInt(), inputMessage2.toInt());
+    _ui_state_modify(ui_firstButton, LV_STATE_CHECKED, (inputMessage2.toInt()+1));
+    ledState = !ledState;
+  }
+  else {
+    inputMessage1 = "No message sent";
+    inputMessage2 = "No message sent";
+  }
+  //Serial.print("GPIO: ");
+  Serial.print(inputMessage1);
+  Serial.print(" - : ");
+  Serial.println(inputMessage2);
+  request->send(200, "text/plain", "OK");
+});
+
+server.on("/state", HTTP_GET, [] (AsyncWebServerRequest *request)
+{
+  request->send(200, "text/plaint", String(lv_obj_get_state(ui_firstButton)).c_str());
+});
+
+  server.begin();
+
   ui_init();
 }
 
 void loop()
 {
   lv_timer_handler(); /* let the GUI do its work */
-  server.handleClient();
 
-  if(btn1Status != Flag1) {
-    Flag1 = btn1Status;
-    if(btn1Status)
-    {
-      _ui_state_modify(ui_firstButton, LV_STATE_CHECKED,_UI_MODIFY_STATE_TOGGLE);
-    }else{
-      _ui_state_modify(ui_firstButton, LV_STATE_CHECKED,_UI_MODIFY_STATE_REMOVE);
+  int flag1 = lv_obj_get_state(ui_firstButton);
+
+  //Serial.println(flag1);
+
+  if (flag1 != buttonState) {
+    buttonState = flag1;
+
+    // only toggle the LED if the new button state is HIGH
+    if (buttonState == HIGH) {
+      ledState = !ledState;
     }
   }
-
-  if(btn2Status != Flag2) {
-    Flag2 = btn2Status;
-    if(btn2Status)
-    {
-      _ui_state_modify(ui_secondButton, LV_STATE_CHECKED,_UI_MODIFY_STATE_TOGGLE);
-    }else{
-      _ui_state_modify(ui_secondButton, LV_STATE_CHECKED,_UI_MODIFY_STATE_REMOVE);
-    }
-  }
-
-  if(btn3Status != Flag3) {
-    Flag3 = btn3Status;
-    if(btn3Status)
-    {
-      _ui_state_modify(ui_thirdButton, LV_STATE_CHECKED,_UI_MODIFY_STATE_TOGGLE);
-    }else{
-      _ui_state_modify(ui_thirdButton, LV_STATE_CHECKED,_UI_MODIFY_STATE_REMOVE);
-    }
-  }
-
-  if(btn4Status != Flag4) {
-    Flag4 = btn4Status;
-    if(btn4Status)
-    {
-      _ui_state_modify(ui_forthButton, LV_STATE_CHECKED ,_UI_MODIFY_STATE_TOGGLE);
-    }else{
-      _ui_state_modify(ui_forthButton, LV_STATE_CHECKED ,_UI_MODIFY_STATE_REMOVE);
-    }
-  }
-
+  Serial.println(ledState);
+  
   tft.setBrightness(brightnessValue); //change screen brightness according to slider's value
 
   //set screen bg color acording to color wheel selected
@@ -264,127 +310,12 @@ void loop()
   delay( 5 );
 }
 
-void handle_OnConnect()
-{
-  btn1Status = LOW;
-  btn2Status = LOW;
-  btn3Status = LOW;
-  btn4Status = LOW;
-  Serial.println("Living room light off | Kitchen room light off");
-  server.send(200, "text/html", SendHTML(btn1Status, btn2Status, btn3Status, btn4Status));
-}
+// void ui_event_firstButton(lv_event_t * e)
+// {
+//   lv_event_code_t event_code = lv_event_get_code(e);
+//   lv_obj_t * target = lv_event_get_target(e);
+//   if(event_code == LV_EVENT_VALUE_CHANGED)
+//   {
 
-void handle_btn1on() {
-  btn1Status = HIGH;
-  Serial.println("Living room light on");
-  server.send(200, "text/html", SendHTML(true, btn2Status, btn3Status, btn4Status));
-}
-
-void handle_btn2on() {
-  btn2Status = HIGH;
-  Serial.println("Kitchen room light on");
-  server.send(200, "text/html", SendHTML(btn1Status, true, btn3Status, btn4Status));
-}
-
-void handle_btn3on() {
-  btn3Status = HIGH;
-  Serial.println("Curtain mode on");
-  server.send(200, "text/html", SendHTML(btn1Status, btn2Status, true, btn4Status));
-}
-
-void handle_btn4on() {
-  btn4Status = HIGH;
-  Serial.println("AC on");
-  server.send(200, "text/html", SendHTML(btn1Status, btn2Status, btn3Status, true));
-}
-
-void handle_btn1off() {
-  btn1Status = LOW;
-  Serial.println("Living room light off");
-  server.send(200, "text/html", SendHTML(false, btn2Status, btn3Status, btn4Status));
-}
-
-void handle_btn2off() {
-  btn2Status = LOW;
-  Serial.println("Kitchen room light off");
-  server.send(200, "text/html", SendHTML(btn1Status, false, btn3Status, btn4Status));
-}
-
-void handle_btn3off() {
-  btn3Status = LOW;
-  Serial.println("Curtain mode off");
-  server.send(200, "text/html", SendHTML(btn1Status, btn2Status, false, btn4Status));
-}
-
-void handle_btn4off() {
-  btn4Status = LOW;
-  Serial.println("AC off");
-  server.send(200, "text/html", SendHTML(btn1Status, btn2Status, btn3Status, false));
-}
-
-void handle_NotFound() {
-  server.send(404, "text/plain", "Not found");
-}
-
-void ui_event_firstButton(lv_event_t * e)
-{
-    lv_event_code_t code = lv_event_get_code(e);
-    lv_obj_t *btn = lv_event_get_target(e);
-    if (code == LV_EVENT_VALUE_CHANGED)
-    {
-      static uint8_t cnt = 0;
-      cnt++;
-
-      if(cnt % 2 ==0)
-      {
-        server.send(200, "text/html", SendHTML(false, btn2Status, btn3Status, btn4Status));
-      }
-      else{
-        server.send(200, "text/html", SendHTML(true, btn2Status, btn3Status, btn4Status));
-      }
-      
-    }
-}
-
-String SendHTML(uint8_t btn1stat, uint8_t btn2stat, uint8_t btn3stat, uint8_t btn4stat){
-  String ptr = "<!DOCTYPE html> <html>\n";
-  ptr +="<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
-  ptr +="<title>ESP32 Control</title>\n";
-  ptr +="<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
-  ptr +="body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;} h3 {color: #444444;margin-bottom: 50px;}\n";
-  ptr +=".button {display: block;width: 80px;background-color: #1abc9c;border: none;color: white;padding: 13px 30px;text-decoration: none;font-size: 25px;margin: 0px auto 35px;cursor: pointer;border-radius: 4px;}\n";
-  ptr +=".button-on {background-color: #1abc9c;}\n";
-  ptr +=".button-on:active {background-color: #16a085;}\n";
-  ptr +=".button-off {background-color: #34495e;}\n";
-  ptr +=".button-off:active {background-color: #2c3e50;}\n";
-  ptr +="p {font-size: 14px;color: #888;margin-bottom: 10px;}\n";
-  ptr +="</style>\n";
-  ptr +="</head>\n";
-  ptr +="<body>\n";
-  ptr +="<h1>ESP32 Web Server</h1>\n";
-  ptr +="<h3>Using Station(STA) Mode</h3>\n";
-  
-  if(btn1stat)
-  {ptr +="<p>Living Room light: ON</p><a class=\"button button-off\" href=\"/btn1off\">OFF</a>\n";}
-  else
-  {ptr +="<p>Living Room light: OFF</p><a class=\"button button-on\" href=\"/btn1on\">ON</a>\n";}
-
-  if(btn2stat)
-  {ptr +="<p>Kitchen Room light: ON</p><a class=\"button button-off\" href=\"/btn2off\">OFF</a>\n";}
-  else
-  {ptr +="<p>Kitchen Room light: OFF</p><a class=\"button button-on\" href=\"/btn2on\">ON</a>\n";}
-
-  if(btn3stat)
-  {ptr +="<p>Curtain mode: ON</p><a class=\"button button-off\" href=\"/btn3off\">OFF</a>\n";}
-  else
-  {ptr +="<p>Curtain mode: OFF</p><a class=\"button button-on\" href=\"/btn3on\">ON</a>\n";}
-
-  if(btn4stat)
-  {ptr +="<p>AC ON</p><a class=\"button button-off\" href=\"/btn4off\">OFF</a>\n";}
-  else
-  {ptr +="<p>AC OFF</p><a class=\"button button-on\" href=\"/btn4on\">ON</a>\n";}
-
-  ptr +="</body>\n";
-  ptr +="</html>\n";
-  return ptr;
-}
+//   }
+// }
